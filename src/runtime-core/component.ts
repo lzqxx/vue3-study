9bc10c64174d7f133bfb4c6f676a096901926108
lzqxx <lzqxxlan@163.com>
2022-04-16 22:08:27 +0800

    feat: getCurrentInstance 方法实现

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -35,10 +35,12 @@ function setupStatefulComponent(instance: any) {
   const { setup } = Component;
 
   if (setup) {
+    setCurrentInstance(instance);
     // props 浅只读
     const setupResult = setup(shallowReadonly(instance.props), {
       emit: instance.emit,
     });
+    setCurrentInstance(null);
     handleSetupResult(instance, setupResult);
   }
 }
@@ -59,4 +61,14 @@ function finishComponentSetup(instance: any) {
   if (Component.render) {
     instance.render = Component.render;
   }
+}
+
+let currentInstance: object | null = null;
+export function getCurrentInstance(): object | null {
+  return currentInstance;
+}
+
+// 全局变量赋值通过函数方式，方便后续问题排查、断点调试
+export function setCurrentInstance(instance: object | null) {
+  currentInstance = instance;
 }
